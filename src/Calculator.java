d3558b8d1628bf4eb940123d44f9e24510d0b4eb
장호영 <ghdud0474@gmail.com>
2025-10-15 18:53:07 +0900

    테스트1

---
+++ src/Calculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("첫 번째 입력 값 : ");
+        int first = scanner.nextInt();
+        System.out.println(first);
+        // TODO 두 번째 값을 입력 받아 저장
+        System.out.println("두 번째 입력 값 : ");
+        int second = scanner.nextInt();
+        System.out.println(second);
+        // TODO 입력 받은 두 값에 대한 사칙연산을 계산해 출력
+        System.out.println("두 값의 덧셈 :" + (first + second));
+        System.out.println("두 값의 뺄셈 :" + (first - second));
+        System.out.println("두 값의 곱셈 :" + (first * second));
+        System.out.println("두 값의 나눗셈 :" + (first / second));
+
+    }
+}
---
+++ src/Calculator2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Calculator2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("첫 번째 입력 값 : ");
+        int first = scanner.nextInt();
+        System.out.println(first);
+
+        System.out.println("두 번째 입력 값 : ");
+        int second = scanner.nextInt();
+        System.out.println(second);
+
+        System.out.println("원하는 기호를 입력하세요 : ");
+        String symbol = scanner.next();
+
+        if ("+".equals(symbol)) {
+            System.out.println("두 값의 덧셈 : " + (first + second));
+        } else if ("-".equals(symbol)) {
+            System.out.println("두 값의 뺄셈 : " + (first - second));
+        } else if ("*".equals(symbol)) {
+            System.out.println("두 값의 곱셈 : " + (first * second));
+        } else if ("/".equals(symbol)) {
+            System.out.println("두 값의 나눗셈 : " + (first / second));
+        } else {
+            System.out.println("사칙연산에 맞지 않는 값을 입력했습니다.");
+        }
+    }
+}
